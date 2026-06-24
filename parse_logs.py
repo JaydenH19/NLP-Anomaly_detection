b719e2a2d95a3f00ef5f0858958bf0fae97c4290
Jayden <97096023@st.deltion.nl>
2026-06-24 11:09:40 +0200

    Removing spaces

--- parse_logs.py
+++ parse_logs.py
@@ -8,6 +8,10 @@ with open('Data/SSH.log', 'r') as file:
 
 df = pd.DataFrame(line, columns=['message'])  
 df ['content'] = df['message'].str.rsplit(":", n=1, expand=True)[1]
+
+
+df['content'] = df['content'].str.strip()
 df['content'].to_csv("Data/cleaned.csv", index=False)
+
 print(df)
 print(df.shape) 
