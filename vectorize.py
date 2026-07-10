5df1b41174f52dc1e79b6e061f32dbf3027efc7e
Jayden <97096023@st.deltion.nl>
2026-07-10 11:21:26 +0200

    Fix NaN error using .fillna('')

--- vectorize.py
+++ vectorize.py
@@ -1,2 +1,10 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
+df = pd.read_csv('Data/cleaned.csv')
+
+vectorizer = TfidfVectorizer()
+print(df['content'].isnull().sum())
+df['content'] = df['content'].fillna('')
+X = vectorizer.fit_transform(df['content'])
+print(X.shape)
