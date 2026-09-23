bc436cb39ca070cfd49415eb055600cde85b1fc1
Jayden <97096023@st.deltion.nl>
2026-09-23 15:41:27 +0200

    Precision, recall, F1 and start confusion matrix

--- train_model.py
+++ train_model.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.ensemble import IsolationForest
 import joblib
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
+
 
 X = joblib.load('Data/IF_X.pkl') # data in number
 
@@ -25,3 +27,15 @@ print((y_pred == -1))
 
 is_anomaly = y_pred == -1 #saves the True/False array in variabele
 print(df_test[is_anomaly])
+
+print("Precision:", precision_score(y_test, is_anomaly)) # print the Precision score (10.4%)
+print("Recall:", recall_score(y_test, is_anomaly)) # print the Recall score (10.5%)
+print("F1 score:", f1_score(y_test, is_anomaly)) # print the F1 score (10.5%) Going to fix it later to get higer scores. 
+
+actual = y_test
+
+predicted = is_anomaly
+
+cm = confusion_matrix(actual, predicted)
+
+print("confusion matrix: ", cm)
