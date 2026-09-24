02feb693bb179e6fa2bcfc61d6b1535bbb3aa2cf
Jayden <97096023@st.deltion.nl>
2026-09-24 11:13:39 +0200

    adding authentication, PAM, Connection for better accuracy. from 10% to almost 60% on the F1

--- train_model.py
+++ train_model.py
@@ -8,7 +8,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, confusion_m
 X = joblib.load('Data/IF_X.pkl') # data in number
 
 df = pd.read_csv('Data/cleaned.csv') # load readable tekst 
-suspicious = df['content'].str.contains('Failed|Invalid|BREAK-IN', case=False, na=False) # True False array
+suspicious = df['content'].str.contains('Failed|Invalid|BREAK-IN|authentication|PAM|Connection', case=False, na=False) # True False array
 print(suspicious.sum()) #print the True lines
 print(len(df))
 
@@ -39,3 +39,6 @@ predicted = is_anomaly
 cm = confusion_matrix(actual, predicted)
 
 print("confusion matrix: ", cm)
+
+is_fp = is_anomaly & ~y_test 
+print(df_test[is_fp])
